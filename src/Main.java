c0073808270831be3bcd52f055752872b37539d8
unknown <vaibhavpatil100008@gmail.com>
2022-07-17 12:02:33 +0530

    multiplication done

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.addTwoNum(10.5,15.5);
+        Main.multiplication(10,10);
 
     }
     public double addTwoNum(double a,double b){
         double c = a+b;
         System.out.println(c);
         return c;
+    }
+    public static int multiplication(int x, int y){
+        int z = x*y;
+        System.out.println(z);
+        return z;
     }
 }
