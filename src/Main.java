2d01e0b2d6610cf841852d0006e99c4d2b7b20f7
unknown <vaibhavpatil100008@gmail.com>
2022-07-17 11:52:46 +0530

    programm done

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.addTwoNum(10.5,15.5);
+
+    }
+    public double addTwoNum(double a,double b){
+        double c = a+b;
+        System.out.println(c);
+        return c;
+    }
+}
